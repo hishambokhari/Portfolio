b4a06bc47412db3b850b3fcbbcd494d5c9a7e7c7
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-20 14:02:19 +0500

    Add: Edit and update functionality

--- app/controllers/git_portfolios_controller.rb
+++ app/controllers/git_portfolios_controller.rb
@@ -7,7 +7,7 @@ class GitPortfoliosController < ApplicationController
   def new
     @portfolio_item = GitPortfolio.new
   end
-
+  
   def create
     @portfolio_item = GitPortfolio.new(params.require(:git_portfolio).permit(:title, :subtitle, :body))
 
@@ -18,5 +18,21 @@ class GitPortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  def update
+    @portfolio_item = GitPortfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:git_portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to git_portfolios_path , notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = GitPortfolio.find(params[:id])
   end
 end
