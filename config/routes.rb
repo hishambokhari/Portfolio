aabd55336cc5c6291bdfdfafd396b4095f453aad
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-20 15:47:42 +0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :git_portfolios
-
+  
+  resources :git_portfolios, except: [:show]
+  get 'git_portfolios/:id', to: 'git_portfolios#show', as: 'git_portfolio_show'
+  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
