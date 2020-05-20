b06e46465a0ef498b8e51e2b3820b89e83f331f3
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-20 13:31:44 +0500

    New and create actions for portoflios

--- app/controllers/git_portfolios_controller.rb
+++ app/controllers/git_portfolios_controller.rb
@@ -3,4 +3,20 @@ class GitPortfoliosController < ApplicationController
   def index
     @portfolio_items = GitPortfolio.all
   end
+
+  def new
+    @portfolio_item = GitPortfolio.new
+  end
+
+  def create
+    @portfolio_item = GitPortfolio.new(params.require(:git_portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to git_portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
