9695aeabe01c5b70c8503ee7c22f7d01cb56f1dc
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-25 15:32:46 +0500

    Customized registeration, login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :git_portfolios, except: [:show]
   get 'angular-items', to: 'git_portfolios#angular'
   get 'git_portfolios/:id', to: 'git_portfolios#show', as: 'git_portfolio_show'
